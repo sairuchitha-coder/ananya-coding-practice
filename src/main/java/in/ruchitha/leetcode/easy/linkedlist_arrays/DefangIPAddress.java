e3f9cd537f23266edcb0559b6542118f7f07531f
ananya-coding-practice <gsairuchitha2001@gmail.com>
2021-05-14 12:09:29 +0530

    DefangIPAddress

---
+++ src/main/java/in/ruchitha/leetcode/easy/linkedlist_arrays/DefangIPAddress.java
@@ -0,0 +1,15 @@
+package in.ruchitha.leetcode.easy.linkedlist_arrays;
+import java.util.Scanner;
+
+
+public class DefangIPAddress {
+    public static void main(String[] args) {
+        DefangIPAddress obj = new DefangIPAddress();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(obj.defangIPaddr(scanner.next()));
+    }
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
+
