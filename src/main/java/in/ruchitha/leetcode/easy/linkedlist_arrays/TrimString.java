32c34d872d463c6a7533058f8756698694a45f65
ananya-coding-practice <gsairuchitha2001@gmail.com>
2021-06-12 15:55:34 +0530

    TrimString

---
+++ src/main/java/in/ruchitha/leetcode/easy/linkedlist_arrays/TrimString.java
@@ -0,0 +1,13 @@
+package in.ruchitha.leetcode.easy.linkedlist_arrays;
+import java.util.Scanner;
+
+class TestString{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a String: ");
+        String s = scanner.next();
+        System.out.println(s.trim());
+    }
+
+}
